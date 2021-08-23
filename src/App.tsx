9018063ccba4be416b6168b9833d731bf4d6ec0d
zihan520 <1145131890@qq.com>
2021-08-23 14:32:03 +0800

    jira项目代码提交

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+// import { ProjectList } from './screens/project-list';
+// import PIndex  from './screens/PIndex'
+import { TsReactTest } from './screens/TryUseArray'
+function App() {
+  return (
+    <div className="App">
+      <TsReactTest />
+    </div>
+  );
+}
+
+export default App;
