305f6cbb9b910542d77c8d4940e48a1a6d3fed3d
zihan520 <1145131890@qq.com>
2021-08-24 18:09:06 +0800

    加入context的使用

---
+++ src/auth-provider.ts
@@ -0,0 +1,44 @@
+import { User } from "./screens/project-list/search-panel";
+const apiURL = process.env.REACT_APP_API_URL;
+const localStorageKey = "__auth_provider_token__";
+export const getToken = () => {
+  window.localStorage.getItem(localStorageKey);
+};
+
+export const handleUseResponse = ({ user }: { user: User }) => {
+  window.localStorage.setItem(localStorageKey, user.token || "");
+  return user;
+};
+
+export const login = (data: { username: string; password: string }) => {
+  return fetch(`${apiURL}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then(async (res) => {
+    if (res.ok) {
+      return handleUseResponse(await res.json());
+    } else {
+      return Promise.reject(await res.json());
+    }
+  });
+};
+export const register = (data: { username: string; password: string }) => {
+  return fetch(`${apiURL}/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then(async (res) => {
+    if (res.ok) {
+      return handleUseResponse(await res.json());
+    } else {
+      return Promise.reject(await res.json());
+    }
+  });
+};
+export const logout = async () =>
+  window.localStorage.removeItem(localStorageKey);
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'antd-mobile/dist/antd-mobile.css';
-import {loadDevTools } from 'jira-dev-tool'
-loadDevTools(()=>ReactDOM.render(
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "antd-mobile/dist/antd-mobile.css";
+import { loadDevTools } from "jira-dev-tool";
+import { AppProviders } from "./context/index";
+loadDevTools(() =>
+  ReactDOM.render(
     <React.StrictMode>
+      <AppProviders>
         <App />
+      </AppProviders>
     </React.StrictMode>,
-    document.getElementById('root')
-))
+    document.getElementById("root")
+  )
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
