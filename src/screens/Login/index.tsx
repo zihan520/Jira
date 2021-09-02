985ac632854c544318dc56a58d5ae06f88c33fdc
zihan520 <1145131890@qq.com>
2021-09-02 17:48:29 +0800

    ts配置baseUrl

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,5 +1,5 @@
 import React, { FormEvent } from "react";
-import { useAuth } from "../../context/auth-context";
+import { useAuth } from "context/auth-context";
 export const LoginScreen = () => {
   const { login, user } = useAuth();
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
