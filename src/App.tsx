6bcd7a671c6cd7353519a8edb449e8426c05813d
zihan520 <1145131890@qq.com>
2021-09-03 18:00:45 +0800

    styles:优化注册登录页面，安装emotion

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 // import { ProjectList } from './screens/project-list';
 // import PIndex  from './screens/PIndex'
-import { TsReactTest } from './screens/TryUseArray'
-import { LoginScreen } from './screens/Login'
+import {TsReactTest} from './screens/TryUseArray'
+import UnauthenticatedApp from './unauther-app'
 function App() {
-  return (
-    <div className="App">
-      {/*<TsReactTest />*/}
-      <LoginScreen />
-    </div>
-  );
+    return (
+        <div className="App">
+            <UnauthenticatedApp/>
+        </div>
+    );
 }
 
 export default App;
