66af90a333802740d6b4cfa9620251922d9862c1
wangdingyu <1145131890@qq.com>
2021-09-19 21:17:33 +0800

    代码提交

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -2,14 +2,54 @@ import React, {Component} from 'react';
 import {ProjectList} from 'screens/project-list';
 import {useAuth} from 'context/auth-context';
 import styled from '@emotion/styled'
+import { constants } from 'os';
 // import { Button, Dropdown, Menu } from "antd";
 export const AuthenticatedApp = ()=>{
   const { logout } = useAuth();
   return(<Container>
-      <button onClick={logout}>登出</button>
+      <Header>
+        <HeaderLeft>
+          <h3>logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
+        <HeaderRight>
+        <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Nav>nav</Nav>
+      <Main>
       <ProjectList />
+      </Main>
+      <Aside>aside</Aside>
+      <Footer>footer</Footer>
   </Container>)
 }
 const Container = styled.div`
-display:grid
+  display:grid;
+  grid-template-rows: 6rem 1fr 6rem;
+  grid-template-columns:20rem 1fr 20rem;
+  grid-template-areas: "header header header"
+  "nav main aside"
+  "footer footer footer";
+  height:100vh;
 `
+const Header = styled.header`
+  grid-area:header;
+  display:flex;
+  flex-dirextion:row;
+  align-items:center;
+  justify-content:space-between
+`
+const HeaderLeft = styled.div`
+display:flex;
+align-items:center;
+`
+const HeaderRight = styled.div`
+
+`
+
+const Aside = styled.aside``
+const Nav = styled.nav``
+const Main = styled.main``
+const Footer = styled.footer``
